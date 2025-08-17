519c604c390cc1d57af472600cfeb6763243421b
zackozack <id.ayushkryadav@gmail.com>
2025-08-17 12:58:11 +0530

    feat: add api key management

--- components/model-selector.tsx
+++ components/model-selector.tsx
@@ -3,7 +3,7 @@
 import { startTransition, useMemo, useOptimistic, useState } from 'react';
 
 import { saveChatModelAsCookie } from '@/app/(chat)/actions';
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -16,6 +16,9 @@ import { cn } from '@/lib/utils';
 import { CheckCircleFillIcon, ChevronDownIcon } from './icons';
 import { entitlementsByUserType } from '@/lib/ai/entitlements';
 import type { Session } from 'next-auth';
+import Image from 'next/image';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { ExternalLinkIcon, InfoIcon } from 'lucide-react';
 
 export function ModelSelector({
   session,
@@ -31,9 +34,9 @@ export function ModelSelector({
 
   const userType = session.user.type;
   const { availableChatModelIds } = entitlementsByUserType[userType];
-
   const availableChatModels = chatModels.filter((chatModel) =>
     availableChatModelIds.includes(chatModel.id),
+
   );
 
   const selectedChatModel = useMemo(
@@ -58,6 +61,13 @@ export function ModelSelector({
           variant="outline"
           className="md:px-2 md:h-[34px]"
         >
+          <Image
+            className="size-5 rounded-full bg-white"
+            src={selectedChatModel?.providerDetails.iconUrl || ''}
+            alt={selectedChatModel?.name || 'Model Icon'}
+            width={24}
+            height={24}
+          />
           {selectedChatModel?.name}
           <ChevronDownIcon />
         </Button>
@@ -85,11 +95,36 @@ export function ModelSelector({
                 type="button"
                 className="gap-4 group/item flex flex-row justify-between items-center w-full"
               >
-                <div className="flex flex-col gap-1 items-start">
-                  <div>{chatModel.name}</div>
-                  <div className="text-xs text-muted-foreground">
-                    {chatModel.description}
-                  </div>
+                <div className="flex flex gap-2 items-center">
+                  <Image className='size-6 rounded-full bg-white' src={chatModel.providerDetails.iconUrl} alt={chatModel.name} width={40} height={40} />
+                  <div className='text-base'>{chatModel.name}</div>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger className='cursor-default'>
+                      <InfoIcon className='size-4 text-muted-foreground' />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <div className='max-w-[250px] text-left space-y-1'>
+                        <div className='flex items-start gap-2'>
+                          <a href={chatModel.providerDetails.url} className='flex flex-col items-center w-fit'>
+                          <Image className='size-8 rounded-full bg-white' src={chatModel.providerDetails.iconUrl} alt={chatModel.name} width={40} height={40} />
+                          <p>{chatModel.providerDetails.name}</p>
+                          </a>
+                          <div>
+                            <p>{chatModel.name}</p>
+                            <p className='text-xs text-muted-foreground'>${chatModel.priceInUsd.input} {chatModel.priceInUsd.unit} Input</p>
+                            <p className='text-xs text-muted-foreground'>${chatModel.priceInUsd.output} {chatModel.priceInUsd.unit} Output</p>
+                          </div>
+                        </div>
+                        <p className='text-xs'>{chatModel.description}</p>
+                        <div>
+                          <div className='flex items-center justify-between'>
+                            <p className='text-xs text-muted-foreground'>{new Intl.NumberFormat('en-US').format(chatModel.contextWindow || 0)} context window</p>
+                            <a className={cn(buttonVariants({ variant: 'link' }), "p-0 h-fit w-fit")} href={chatModel.modelUrl}>Read more <ExternalLinkIcon className='size-3 text-muted-foreground' /></a>
+                          </div>  
+                        </div>
+                      </div>
+                    </TooltipContent>
+                  </Tooltip>
                 </div>
 
                 <div className="text-foreground dark:text-foreground opacity-0 group-data-[active=true]/item:opacity-100">
--- lib/ai/entitlements.ts
+++ lib/ai/entitlements.ts
@@ -3,6 +3,10 @@ import type { ChatModel } from './models';
 
 interface Entitlements {
   maxMessagesPerDay: number;
+  /**
+   * List of available chat model IDs for the user type.
+   * Leave unspecified to allow all models.
+   */
   availableChatModelIds: Array<ChatModel['id']>;
 }
 
@@ -11,19 +15,20 @@ export const entitlementsByUserType: Record<UserType, Entitlements> = {
    * For users without an account
    */
   guest: {
-    maxMessagesPerDay: 20,
-    availableChatModelIds: ['chat-model', 'chat-model-reasoning'],
+    maxMessagesPerDay: 2000000,
+    availableChatModelIds: ['gpt-5-nano'],
   },
 
   /*
    * For users with an account
+  * Allow all models, but limit messages per day
    */
   regular: {
-    maxMessagesPerDay: 100,
-    availableChatModelIds: ['chat-model', 'chat-model-reasoning'],
+    maxMessagesPerDay: 10,
+    availableChatModelIds: ['gpt-5-nano'],
   },
 
   /*
-   * TODO: For users with an account and a paid membership
+   * TODO: For users with an account and has their own API key
    */
 };
